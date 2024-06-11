44e6063fd4a460843865457de0f14a8b77eb19bf
BenPyton <ben.pyton@gmail.com>
2024-06-11 16:28:54 +0200

    Updated license notices to BSL-1.0

--- Source/DungeonRulesEditor/Private/NodeSlates/SGraphNodeDungeonRuleTransition.cpp
+++ Source/DungeonRulesEditor/Private/NodeSlates/SGraphNodeDungeonRuleTransition.cpp
@@ -1,4 +1,7 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Copyright (c) 2024 Benoit Pelletier
+// SPDX-License-Identifier: BSL-1.0
+// Distributed under the Boost Software License, Version 1.0. 
+// (See accompanying file LICENSE or copy at https://www.boost.org/LICENSE_1_0.txt)
 
 
 #include "SGraphNodeDungeonRuleTransition.h"
--- Source/DungeonRulesEditor/Private/Toolkits/DungeonRulesToolkit.cpp
+++ Source/DungeonRulesEditor/Private/Toolkits/DungeonRulesToolkit.cpp
@@ -1,3 +1,8 @@
+// Copyright (c) 2024 Benoit Pelletier
+// SPDX-License-Identifier: BSL-1.0
+// Distributed under the Boost Software License, Version 1.0. 
+// (See accompanying file LICENSE or copy at https://www.boost.org/LICENSE_1_0.txt)
+
 #include "DungeonRulesToolkit.h"
 #include "DungeonRulesEdLog.h"
 #include "DungeonRulesEdTypes.h"
